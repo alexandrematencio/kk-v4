90e4347bd412155ade40e6442adffe4e42bed5ca
Alexandre Matencio <amatencio@pm.me>
2026-05-20 12:05:49 +0200

    deploy

---
+++ _nuxt/DNk2c_OS.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c as r,C as n}from"./DI64KvbV.js";const t={};function c(e,a){return s(),r("ol",null,[n(e.$slots,"default")])}const _=Object.assign(o(t,[["render",c]]),{__name:"ProseOl"});export{_ as default};
